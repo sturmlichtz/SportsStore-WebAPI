622d208c3bb102c6e6b732e048b4b96f333a800c
Maheshkumar N <pn.maheshkumar@gmail.com>
2021-06-30 15:52:04 +0530

    DbContext

---
+++ SSWebAPIApp/Models/SportsStoreDbContext.cs
@@ -0,0 +1,25 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using SSWebAPIApp.Models.Entities;
+
+namespace SSWebAPIApp.Models
+{
+  public class SportsStoreDbContext : DbContext
+  {
+    public SportsStoreDbContext(DbContextOptions<SportsStoreDbContext> dbContextOptions) : base(dbContextOptions) { }
+
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderDetail> OrderDetails { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      #region Default
+      //base.OnModelCreating(modelBuilder); 
+      #endregion
+
+      // Create Composite primary key in OrderDetail
+      modelBuilder.Entity<OrderDetail>().HasKey(od => new { od.OrderId, od.ProductId });
+    }
+  }
+}
